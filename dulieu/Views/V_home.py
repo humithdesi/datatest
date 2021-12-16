1a86c1b5717d2d13751f8c69ee1d8ec10226a402
thang <huynhminhthang534@gmail.com>
2021-12-16 23:54:12 +0700

    test

--- dulieu/Views/V_home.py
+++ dulieu/Views/V_home.py
@@ -3,5 +3,6 @@ from ..Models.models import HocSinh
 # Create your views here.
 def Home(request):
     hosinhs=HocSinh.objects.all()
+    print('ok thanh cong roi')
     content={'hocsinhs':hosinhs}
     return render(request,'home.htm',content)
