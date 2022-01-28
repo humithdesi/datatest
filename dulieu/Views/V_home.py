9fa36bbed9d28f3fbcafa453b6bd3706d33aba4d
thang <huynhminhthang534@gmail.com>
2022-01-28 18:52:15 +0700

    asd

--- dulieu/Views/V_home.py
+++ dulieu/Views/V_home.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse,Http404
 # Create your views here.
 def Home(request):
-    try:
         return render(request,'home.htm')
-    except:
-        raise Http404('Trang Không tồn tại')
