2798ee0b0ea28faca0617c1022f53bc23cf2982e
thang <huynhminhthang534@gmail.com>
2022-01-29 12:42:17 +0700

    edit homtag

--- dulieu/templatetags/tag_HomeSlide.py
+++ dulieu/templatetags/tag_HomeSlide.py
@@ -3,20 +3,20 @@ from ..Models.M_Product import Product
 
 register=template.Library()
 
-@register.inclusion_tag('datatest/Hometag/HomeSlide1.htm',)
+@register.inclusion_tag('datatest/hometag/HomeSlide1.htm',)
 def HomeSlide1():
 
     posts=0
     content = {'posts': posts}
     return content
 
-@register.inclusion_tag('datatest/Hometag/HomeSlide2.htm',)
+@register.inclusion_tag('datatest/hometag/HomeSlide2.htm',)
 def HomeSlide2():
     products=Product.objects.all()[:4]
     content={'products':products}
     return content
 
-@register.inclusion_tag('datatest/Hometag/HomeSlide3.htm',)
+@register.inclusion_tag('datatest/hometag/HomeSlide3.htm',)
 def HomeSlide3():
     products=Product.objects.all()[:4]
     content={'products':products}
