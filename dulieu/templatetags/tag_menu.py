e31acb26244aebf25d37054fb496643e499bb3f4
humithdesi <huynhminhthang534@gmail.com>
2022-03-19 23:53:42 +0700

    camket1

--- dulieu/templatetags/tag_menu.py
+++ dulieu/templatetags/tag_menu.py
@@ -4,6 +4,6 @@ register=template.Library()
 
 @register.inclusion_tag('dulieu/menu/Menu.htm',takes_context=True)
 def Menu(context):
-    user=context['user'] # muốn dùng user trong thẻ mẫu inclusion_tag phải gọi bằng context
+    user=context['user'] # muốn dùng user trong thẻ mẫu inclusion_tag phải gọi bằng contexts
     content={'user':user}
     return content
